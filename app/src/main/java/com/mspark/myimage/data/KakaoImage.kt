da4bbd5e69925ee6410c1a2522b65d5fb55343cc
MSPARKK <pms93009@gmail.com>
2023-03-23 22:19:10 +0900

    getTimeStamp 로직을 TimeStampUtil로 뺌

--- app/src/main/java/com/mspark/myimage/data/KakaoImage.kt
+++ app/src/main/java/com/mspark/myimage/data/KakaoImage.kt
@@ -1,13 +1,6 @@
 package com.mspark.myimage.data
 
-import android.os.Build
 import com.google.gson.annotations.SerializedName
-import java.text.ParseException
-import java.text.SimpleDateFormat
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-import java.time.format.DateTimeParseException
-import java.util.*
 
 data class KakaoImage(
     @SerializedName("thumbnail_url", alternate = ["thumbnail"])
@@ -21,38 +14,4 @@ data class KakaoImage(
     // @@ 잠깐! todo : test 로직 삭제
     @SerializedName("url")
     val url: String? = null,
-) {
-
-
-    fun getTimeStamp(): String {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            return try {
-                val inputFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX"
-                val outputFormat = "yy.MM.dd HH:mm"
-
-                val formatterInput = DateTimeFormatter.ofPattern(inputFormat)
-                val formatterOutput = DateTimeFormatter.ofPattern(outputFormat)
-
-                val dateTime = LocalDateTime.parse(dateTime, formatterInput)
-
-                return dateTime.format(formatterOutput)
-            } catch (e: DateTimeParseException) {
-                e.printStackTrace()
-                ""
-            }
-        }
-
-        val isoFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS", Locale.getDefault())
-        val desiredFormat = SimpleDateFormat("MM/dd HH:mm", Locale.getDefault())
-
-        return try {
-            val date: Date = (isoFormat.parse(dateTime))?: return ""
-            val formattedDateTime: String = desiredFormat.format(date)
-            formattedDateTime
-
-        } catch (e: ParseException) {
-            e.printStackTrace()
-            ""
-        }
-    }
-}
+)
---
+++ app/src/main/java/com/mspark/myimage/util/TimeStampUtil.kt
@@ -0,0 +1,44 @@
+package com.mspark.myimage.util
+
+import android.os.Build
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
+import java.util.*
+
+object TimeStampUtil {
+    @JvmStatic
+    fun getTimeStamp(dateTime: String): String {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            return try {
+                val inputFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX"
+                val outputFormat = "yy.MM.dd HH:mm"
+
+                val formatterInput = DateTimeFormatter.ofPattern(inputFormat)
+                val formatterOutput = DateTimeFormatter.ofPattern(outputFormat)
+
+                val localDateTime = LocalDateTime.parse(dateTime, formatterInput)
+
+                return localDateTime.format(formatterOutput)
+            } catch (e: DateTimeParseException) {
+                e.printStackTrace()
+                ""
+            }
+        }
+
+        val isoFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS", Locale.getDefault())
+        val desiredFormat = SimpleDateFormat("MM/dd HH:mm", Locale.getDefault())
+
+        return try {
+            val date: Date = (isoFormat.parse(dateTime))?: return ""
+            val formattedDateTime: String = desiredFormat.format(date)
+            formattedDateTime
+
+        } catch (e: ParseException) {
+            e.printStackTrace()
+            ""
+        }
+    }
+}
