02fdb7ad5cbc0c16182a4cdd1fbc88b484e634ea
MSPARKK <pms93009@gmail.com>
2023-03-23 22:25:45 +0900

    getTimeStamp 버전 별로 함수를 나눔

--- app/src/main/java/com/mspark/myimage/util/TimeStampUtil.kt
+++ app/src/main/java/com/mspark/myimage/util/TimeStampUtil.kt
@@ -1,6 +1,7 @@
 package com.mspark.myimage.util
 
 import android.os.Build
+import androidx.annotation.RequiresApi
 import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.time.LocalDateTime
@@ -12,22 +13,31 @@ object TimeStampUtil {
     @JvmStatic
     fun getTimeStamp(dateTime: String): String {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            return try {
-                val inputFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX"
-                val outputFormat = "yy.MM.dd HH:mm"
-
-                val formatterInput = DateTimeFormatter.ofPattern(inputFormat)
-                val formatterOutput = DateTimeFormatter.ofPattern(outputFormat)
-
-                val localDateTime = LocalDateTime.parse(dateTime, formatterInput)
-
-                return localDateTime.format(formatterOutput)
-            } catch (e: DateTimeParseException) {
-                e.printStackTrace()
-                ""
-            }
+            return getTimeStampOreoAndAbove(dateTime)
         }
 
+        return getTimeStampUnderOreo(dateTime)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun getTimeStampOreoAndAbove(dateTime: String): String {
+        return try {
+            val inputFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX"
+            val outputFormat = "yy.MM.dd HH:mm"
+
+            val formatterInput = DateTimeFormatter.ofPattern(inputFormat)
+            val formatterOutput = DateTimeFormatter.ofPattern(outputFormat)
+
+            val localDateTime = LocalDateTime.parse(dateTime, formatterInput)
+
+            return localDateTime.format(formatterOutput)
+        } catch (e: DateTimeParseException) {
+            e.printStackTrace()
+            ""
+        }
+    }
+
+    private fun getTimeStampUnderOreo(dateTime: String): String {
         val isoFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS", Locale.getDefault())
         val desiredFormat = SimpleDateFormat("MM/dd HH:mm", Locale.getDefault())
 
